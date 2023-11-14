1edafff391118d3dff1496d3630de0c07b6244f7
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-14 12:58:21 +0500

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿//DataTypes
+//1) Interger - nartural sonlar bilan ishlash uchun foydalaniladi -2 milliarddan 2 milliardgacha
+//int son = 1000;
+//int son2 = int.Parse(Console.ReadLine());
+//Console.WriteLine(son + son2);
+
+//String - Matnlar bilan ishlassh uchun foydalaniladigan o'zgaruvchi type
+//string text = "Assalomu aleykum! ";
+//string text2 = Console.ReadLine();
+//Console.WriteLine(text + text2);
+
+//Double - qoldiqli va natural sonlarni qabul qiladi -4 miliardan 4 miliardgacha
+//double son = 3.14;
+//double son2 = double.Parse(Console.ReadLine());
+//Console.WriteLine(son * son2);
+
