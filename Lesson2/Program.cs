4fe56319c6ab35cd89bc323e28f15e44fd841d87
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-14 13:55:51 +0500

    Lesson2

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Qoldiqli va natural sonlar uchun ishatiladi -4 milliarddan 4 milliardgacha son qabul qiladi
+//float f1 = 3.14f; //f
+//Console.Write("Xushkelibsiz! radiusni kiriting: ");
+//float f2 = float.Parse(Console.ReadLine());
+//Console.WriteLine($"Natija : {f1 * f2}");
+
+//decimal 128 bit joy egallaydi
+//decimal m1 = 12396131653721427381362751231m;
+//decimal m2 = decimal.Parse(Console.ReadLine());
+//Console.WriteLine(m1 * m2);
+
+//var s = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+//double pi = 3.14;
+//Console.WriteLine(((s[0] + s[1]) * s[2]) / s[3]);
+//Console.WriteLine((s[0] * s[0]) * pi);
