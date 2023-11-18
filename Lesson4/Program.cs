07915c5243886069ef628c2573e4374cc1bd5e7f
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-18 14:00:43 +0500

    Lesson 4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,42 @@
+﻿//Shart operatori
+
+//if, else, else if
+
+//string t1 = Console.ReadLine();
+//if(t1 != null) // not null
+//{
+//    Console.WriteLine($"Salom {t1}");
+//}
+//else
+//{
+//    Console.WriteLine(" ");
+////}   
+//int son = int.Parse(Console.ReadLine());
+
+//if (son > 100)
+//    Console.WriteLine("100 dan katta");
+//else if (son == 100)
+//    Console.WriteLine("100 ga teng");
+//else
+//    Console.WriteLine("100 dan kichik");
+
+//var s = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+//int a = s[0] + s[1];
+//if(a == s[2])
+//    Console.WriteLine(true);
+//else
+//    Console.WriteLine(false);
+
+
+//if (s[0] % 2 == 0)
+//    Console.WriteLine("Juft son");
+//else
+//    Console.WriteLine("Toq son");
+
+//string a = Console.ReadLine();
+
+//if(a.Length == 5)
+//    Console.WriteLine("Matnni uzunligi 5 ta");
+//else
+//    Console.WriteLine("Matn 5 tadan uzun");
