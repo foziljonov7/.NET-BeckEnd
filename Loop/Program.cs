6e8b664cf9db0442cd71605d85d17547095e92bc
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-21 14:01:52 +0500

    Loop

---
+++ Loop/Program.cs
@@ -0,0 +1,41 @@
+﻿// Takrolash operatorlari
+//1. for
+//2. while
+//3. do-while
+//4. foreach
+
+//for (int i = 10; i > 0; i--)
+//{
+//    Console.Write("Kvadratning tomonini kiriting : ");
+//    int s = int.Parse(Console.ReadLine());
+//    int result = s * 4;
+//    Console.WriteLine($"Natija : {result}");
+//}
+//Console.WriteLine("Tugadi!");
+
+//for (int j = 100; j > 0; j--)
+//    Console.WriteLine(j);
+
+//for (int i = 0; i < 1000; i++)//for(boshlangich qimati; operatori; postfiks)
+//    Console.WriteLine($"{i} : .NET BeckEnd group!");
+
+for (int k = 0; k < 10; k++)
+{
+    int a = int.Parse(Console.ReadLine());
+    if (a > 100)
+    {
+        for (int i = a; i >= 0; i--)
+            Console.WriteLine(i);
+    }
+    else if (a <= 10)
+    {
+        for (int j = a; j <= 100; j++)
+            Console.WriteLine(j);
+    }
+    else
+        Console.WriteLine(a);
+}
+
+// Masala natural son kiritilsin, kiritilgan son toq bolsa qiymat 0 gacha kamaysin
+// aks holda 1000 gacha ortsin
+// if(son % 2 == 1){ for } else { for }   
