12dcfd41033b574a275958add7b5717d2e3de7c2
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-28 13:20:42 +0500

    lesson 8, array and foreach

--- Lesson7/Program.cs
+++ Lesson7/Program.cs
@@ -1,29 +1,50 @@
 ﻿// Switch-case Tanlash operatori
 
 
-int i = 0;
-do
-{
-    if(i < 1)
-    {
-        Console.WriteLine("Welcome to ...!");
-        Console.WriteLine("Hozirda menyuda ushbu Fast foodlar mavjud");
-        Console.WriteLine("1: Gamburger");
-        Console.WriteLine("2: Chizburger");
-        Console.WriteLine("3: Lavash");
-    }
-    Console.Write("Taom tanlang : ");
-    string s = Console.ReadLine();
-    switch (s)
-    {
-        case "Gamburger":
-            Console.WriteLine("Gamburger narxi 25000"); break;
-        case "Chizburger":
-            Console.WriteLine("Chizburger narxi 25000"); break;
-        case "Lavash":
-            Console.WriteLine("Lavash narxi 25000"); break;
-        default:
-            Console.WriteLine("Halvol joyidami!"); break;
-    }
-    i++;
-} while (i < 100);
+//int i = 0;
+//do
+//{
+//    if(i < 1)
+//    {
+//        Console.WriteLine("Welcome to ...!");
+//        Console.WriteLine("Hozirda menyuda ushbu Fast foodlar mavjud");
+//        Console.WriteLine("1: Gamburger");
+//        Console.WriteLine("2: Chizburger");
+//        Console.WriteLine("3: Lavash");
+//    }
+//    Console.Write("Taom tanlang : ");
+//    string s = Console.ReadLine();
+//    switch (s)
+//    {
+//        case "Gamburger":
+//            Console.WriteLine("Gamburger narxi 25000"); break;
+//        case "Chizburger":
+//            Console.WriteLine("Chizburger narxi 25000"); break;
+//        case "Lavash":
+//            Console.WriteLine("Lavash narxi 25000"); break;
+//        default:
+//            Console.WriteLine("Ahvol joyidami!"); break;
+//    }
+//    i++;
+//} while (i < 100);
+
+//double n = int.Parse(Console.ReadLine());
+//do
+//{
+//    Console.WriteLine(n + " : Salom");
+//    n++;
+//}while(n < 100);
+
+//do
+//{
+//    string h = Console.ReadLine();
+//    switch(h)
+//    {
+//        case "Hi":
+//            Console.WriteLine("Salom"); break;
+//        case "Hello":
+//            Console.WriteLine("Assalomu aleykum"); break;
+//        default: 
+//            Console.WriteLine("Ahvollar joyidami"); break;
+//    }
+//} while (true);
---
+++ lesson8/Program.cs
@@ -0,0 +1,30 @@
+﻿//Array
+//string[] fruits = { "Apple", "Banana", "percil" };
+//int[] number = new int[] { 1, 2, 3, 4, 5, 6 };
+//double[] d = new double[5];
+////d[0, 1, 2, 3, 4]
+
+//for(int i = 0; i < 5; i++)
+//{
+//    double n = double.Parse(Console.ReadLine());
+//    d[i] = n; //d[i] = 10;
+//}
+
+//foreach(double p in d)
+//    Console.WriteLine(p);
+
+//foreach(string fruit in fruits)
+//    Console.WriteLine(fruit);
+
+//Console.WriteLine(fruits[1]);
+//Console.WriteLine(number[4]);
+
+//var s = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//Console.WriteLine(s[0]);
+
+//string[] shaharlar = new string[] { "Margilon", "Toshloq", "Yazavon" };
+//foreach(string shahar in shaharlar)
+//    Console.WriteLine(shahar);
+
+//for(int i = 2; i >= 0; i--)
+//    Console.WriteLine(shaharlar[i]);
