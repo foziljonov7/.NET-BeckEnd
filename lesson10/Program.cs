053eabd3421f83bf6a863ee8d8be2579aa0b881d
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-12-02 13:28:05 +0500

    Lesson 10 break and continue

---
+++ lesson10/Program.cs
@@ -0,0 +1,43 @@
+﻿//for(int i = 0; i<10; i++) // 0 1 2 3 4 5 6 7 8 9
+//{
+//    for(int n = 0; n < 10; n++) // 0 1 2 3 4 5 6 7 8 9
+//    {
+//        if(n >= i)
+//        {
+//            break;
+//        }
+//        Console.Write(n + " ");
+//    }
+//    Console.WriteLine(i + " ");
+//}
+// 0
+// 0 1
+// 0 1 2
+
+
+//int i = 100;
+//while(i > 0)
+//{
+//    if (i == 50)
+//        break; 
+
+//    Console.WriteLine(i);
+//    i--;
+//}
+//Console.WriteLine("Tugadi!");
+
+
+//int[] arr = { 123, 68712, 12331, 7186361, 12312 };
+//for(int i = 0; i < 5; i++)
+//{
+//    if (arr[i] % 2 == 1) continue; 
+//    Console.WriteLine(arr[i]);
+//}
+
+string t1 = "Assalomu aleykum";
+for(int i = 0; i < 3; i++)
+{
+    string t2 = Console.ReadLine();
+    if (t1 == t2) continue;
+    Console.WriteLine(t1);
+}
