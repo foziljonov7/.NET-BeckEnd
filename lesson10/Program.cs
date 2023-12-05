eae8fbe405a69da327bec717ed94c4d4233eeec0
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-12-05 13:13:42 +0500

    ...

--- lesson10/Program.cs
+++ lesson10/Program.cs
@@ -28,16 +28,46 @@
 
 
 //int[] arr = { 123, 68712, 12331, 7186361, 12312 };
-//for(int i = 0; i < 5; i++)
+//for (int i = 0; i < 5; i++)
 //{
-//    if (arr[i] % 2 == 1) continue; 
+//    if (arr[i] % 2 == 1) continue;
 //    Console.WriteLine(arr[i]);
 //}
 
-string t1 = "Assalomu aleykum";
-for(int i = 0; i < 3; i++)
-{
-    string t2 = Console.ReadLine();
-    if (t1 == t2) continue;
-    Console.WriteLine(t1);
-}
+//string t1 = "Assalomu aleykum";
+//for(int i = 0; i < 3; i++)
+//{
+//    string t2 = Console.ReadLine();
+//    if (t1 == t2) continue;
+//    Console.WriteLine(t1);
+//}
+
+//int son = 20;
+//key:
+//if (son > 20)
+//{
+//    Console.WriteLine($"{son} > 20");
+//}
+//else if (son == 20)
+//{
+//    son++;
+//    goto key;
+//}
+//else
+//{
+//    Console.WriteLine($"{son} < 20");
+//}
+
+
+//string img = Console.ReadLine();
+//key:
+//if(img.Length > 5)
+//{
+//    Console.WriteLine(img);
+//}
+//else
+//{
+//    img += ".jpg";
+//    goto key;
+//}
+
