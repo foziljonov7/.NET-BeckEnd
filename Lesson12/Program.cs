d7fb871a4df26233ff4334f892a47dd0140bc369
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-12-07 13:11:04 +0500

    Metod

---
+++ Lesson12/Program.cs
@@ -0,0 +1,37 @@
+﻿//Modifikatorlar
+//private, public, protected
+//Metodlar
+
+//int AddNumber(int a, int b)
+//{
+//    return a + b;
+//}
+
+//var s = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//Console.WriteLine(AddNumber(s[0], s[1]));
+
+//void Text(string a)
+//{
+//    Console.WriteLine(a + " xush kelibsiz!");
+//}
+
+//string t1 = Console.ReadLine(); //Abdulvosid
+//Text(t1);
+
+//int a = int.Parse(Console.ReadLine());
+//if (a % 2 == 0)
+//    Ortirish(a);
+//else
+//    Kamayish(a);
+
+//void Ortirish(int a)
+//{
+//    for(int i = a; i <= 100; i++)
+//        Console.WriteLine(i);
+//}
+
+//void Kamayish(int a)
+//{
+//    for(int i = a; i >= 0; i--)
+//        Console.WriteLine(i);
+//}
