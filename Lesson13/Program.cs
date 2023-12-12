b27e6cdff388c7418f1dce8f470ece6c873bb79a
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-12-12 13:45:08 +0500

    Lesson 13 ref, out

--- Lesson13/Program.cs
+++ Lesson13/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿//int a = 100;
+//int b = 200;
+//swapp(ref a, ref b);
+//Console.WriteLine(a + " " + b);
+//static void swapp(ref int a, ref int b)
+//{
+//    int k = a;
+//    a = b;
+//    b = k;
+//}
+
+
+int k = int.Parse(Console.ReadLine());
+Kvadrat(k, out int perimetr, out int yuza, out int kub);
+Console.WriteLine(perimetr + " " + yuza + " " + kub);
+
+static void Kvadrat(int k, out int perimetr, out int yuza, out int kub)
+{
+    perimetr = k * 4;
+    yuza = k * k;
+    kub = k * k * k;
+}
+
+
+
+
+
+
+
+
+
+//Console.WriteLine(max(s[0], s[1]));
+//Console.WriteLine(max(s[0], s[1]));
+
+//static int max(int a, int b)
+//{
+//    if (a > b)
+//        return a;
+//    else
+//        return b;
+//}
+
+//static void max2(int a, int b)
+//{
+//    int max;
+//    if (a > b)
+//        max = a;
+//    else
+//        max = b;
+//}
+
