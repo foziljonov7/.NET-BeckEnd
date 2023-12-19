30d9601325d0448004e166d31b9377aa35de4fb8
Abdulvosid Foziljonov <89521523+foziljonov7@users.noreply.github.com>
2023-12-19 13:37:49 +0500

    Lesson 16 Class, property, constructor, metod takrorlash

--- Lesson15/Program.cs
+++ Lesson15/Program.cs
@@ -5,13 +5,25 @@
 //    $"Author: {book.Author} Price: {book.Price}" +
 //    $"  {book.Time}");
 
-Console.Write("Id : ");
+//Console.Write("Id : ");
+//int id = int.Parse(Console.ReadLine());
+//Console.Write("Course name : ");
+//string name = Console.ReadLine();
+//Console.Write("Cuorse teacher : ");
+//string teacher = Console.ReadLine();
+
+//Course course = new Course(id, name, teacher);
+//Console.WriteLine($"{course.Id}  {course.Name}  {course.Teacher}");
+
+Console.Write("Student Id sini kiriting: ");
 int id = int.Parse(Console.ReadLine());
-Console.Write("Course name : ");
-string name = Console.ReadLine();
-Console.Write("Cuorse teacher : ");
-string teacher = Console.ReadLine();
+Console.Write("Student to'liq ismini kiriting: ");
+string fullname = Console.ReadLine();
+Console.Write("Student manzilini kiriting: ");
+string location = Console.ReadLine();
 
-Course course = new Course(id, name, teacher);
-Console.WriteLine($"{course.Id}  {course.Name}  {course.Teacher}");
-
+Student student = new Student(id, fullname, location);
+student.GetStudent();
+Console.Write("Student idsi orqali qidirish: ");
+int studentId = int.Parse(Console.ReadLine());
+student.GetStudentById(studentId);
---
+++ Lesson15/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15
+{
+    public class Student 
+    {
+        public Student(int id, string fullname, string location)
+        {
+            Id = id;
+            Fullname = fullname;
+            Age = 18;
+            Location = location;
+        }
+
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public int Age { get; set; }
+        public string Location { get; set; }
+
+        public void GetStudent()
+        {
+            Console.WriteLine($" Id: {Id} to'liq ismi: {Fullname} " +
+                $" yoshi: {Age} manzili: {Location}");
+        }
+        public void GetStudentById(int id)
+        {
+            if (Id == id)
+                Console.WriteLine($" Id: {Id} to'liq ismi: {Fullname} " +
+                $" yoshi: {Age} manzili: {Location}");
+            else
+                Console.WriteLine("Null");
+        }
+    }
+}
