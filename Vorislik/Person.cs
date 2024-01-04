c6d122609351298e5f661ae96730dd1504cc18f2
Abdulvosid Foziljonov <89521523+foziljonov7@users.noreply.github.com>
2024-01-04 13:19:01 +0500

    Vorislik

---
+++ Vorislik/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vorislik
+{
+    public class Person
+    {
+        private string name { get; set; } //Nabijon
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ Vorislik/Program.cs
@@ -0,0 +1,9 @@
+﻿using Vorislik;
+
+Person p = new Person();
+p.Name = "Nabijon";
+p.Print();
+
+p = new Employee();
+p.Name = "Abdullo";
+p.Print();
